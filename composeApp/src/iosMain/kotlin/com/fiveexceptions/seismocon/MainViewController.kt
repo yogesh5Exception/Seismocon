aef6356fb7cd519d66fc051d07f255fa7ce00d8e
Anurag <anurag.singh@5exceptions.com>
2025-07-15 19:58:39 +0530

    ios full screen

--- composeApp/src/iosMain/kotlin/com/fiveexceptions/seismocon/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/fiveexceptions/seismocon/MainViewController.kt
@@ -1,6 +1,11 @@
 package com.fiveexceptions.seismocon
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.fiveexceptions.seismocon.splash.App
+import com.fiveexceptions.seismocon.dashboard.home.allSensorsNow.AllSensorsNow
+import com.fiveexceptions.seismocon.navigation.App
 
-fun MainViewController() = ComposeUIViewController { App() }
+
+fun MainViewController() = ComposeUIViewController {
+    App()
+//    AllSensorsNow()
+}
